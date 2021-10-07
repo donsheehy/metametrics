ef2d929861fc6a14fc20e345cf8d27c94662c606
Oliver Chubet <oliver.chubet@gmail.com>
2021-10-07 15:35:51 -0400

    __str__ and __repr__ of points

--- metametrics/point.py
+++ metametrics/point.py
@@ -1,13 +1,19 @@
 class Point():
-    def __init__(self, x, y):
-        self.x, self.y = x, y
+		def __init__(self, x, y):
+			self.x, self.y = x, y
 
-    def __iter__(self):
-        yield self.x
-        yield self.y
+		def __iter__(self):
+			yield self.x
+			yield self.y
 
-    def __eq__(self, other):
-        return list(self) == list(other)
+		def __eq__(self, other):
+			return list(self) == list(other)
 
-    def __hash__(self):
-        return hash((self.x, self.y))
+		def __hash__(self):
+			return hash((self.x, self.y))
+
+		def __str__(self):
+			return f"({self.x},{self.y})"
+
+		def __repr__(self):
+			return f"({self.x},{self.y})"
